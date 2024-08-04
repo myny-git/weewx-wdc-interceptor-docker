ffa036dafb24cf703bc3449bb55d6f06fab46161
myny-git <81964288+myny-git@users.noreply.github.com>
2024-08-04 17:14:23 +0200

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -11,6 +11,16 @@ service rsyslog start
 #echo 'Starting syslog'
 #/sbin/syslogd -n -S -O - &
 
+echo 'copy weewx.conf file'
+# Copy custom weewx.conf if it exists
+if [ -f "${WEEWX_HOME}/data/weewx.conf" ]; then
+    echo "Using custom weewx.conf"
+    cp "${WEEWX_HOME}/data/weewx.conf" "${WEEWX_HOME}/weewx.conf"
+else
+    echo "Custom weewx.conf not found, using default"
+    cp "${WEEWX_HOME}/weewx${WEEWX_VERSION}.conf" "${WEEWX_HOME}/weewx.conf"
+fi
+
 # start weewx
 echo "Starting Weewx version ${WEEWX_VERSION}"
 # shellcheck source=/dev/null
